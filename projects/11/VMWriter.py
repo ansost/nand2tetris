74568fd2630cd4a037bc546c0e81239f732c17a2
ansost <anste145@hhu.de>
2025-07-02 15:22:10 +0200

    more bones for the skeleton

--- projects/11/VMWriter.py
+++ projects/11/VMWriter.py
@@ -1,26 +1,38 @@
 def write_push(segment, index):
-    return
+    return f"push {segment} {index}"
 
 def write_pop(segment, index):
-    return
+    """Segment can be: ARG, LOCAL, STATIC, THIS, THAT, POINTER, TEMP
+    index has to be an integer"""
+    return f"pop {segment} {index}"
 
 def write_arithmetic(command):
-    return
+    """command can be: add, sub, neg, eq, gt, lt, and, or, not"""
+    lookup = {
+    '+': 'ADD',
+    '-': 'SUB',
+    '=': 'EQ',
+    '>': 'GT',
+    '<': 'LT',
+    '&': 'AND',
+    '|': 'OR'
+  }
+    return f"{lookup[command]}\n"
 
 def write_label(label):
-    return
+    return f"label {label}"
 
 def write_goto(label):
-    return
+    return f"goto {label}"
 
 def write_if(label):
     return
 
-def write_routineCall(routineName, args):
-    return
+def write_routineCall(routineName, nArgs):
+    return f"call {routineName} {nArgs}"
 
-def write_functionCall(functionName, args):
-    return
+def write_functionCall(functionName, nVars):
+    return f"function {functionName} {nVars}"
 
 def write_return():
-    return
+    return f"return\n"
